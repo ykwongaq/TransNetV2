1ac88e897b65aa34f3bf892ed398ca310b196c94
wyk <arkwanyuk@gmail.com>
2024-05-23 18:42:24 +0800

    Add debug message

--- inference/inference.py
+++ inference/inference.py
@@ -30,8 +30,8 @@ def main(args):
     for video_filename in os.listdir(video_folder):
         if not video_filename.endswith(".mp4"):
             continue
-        
-            
+
+        print(f"Processing video {video_filename}...")
         video_path = os.path.join(video_folder, video_filename)
         start_time = time.time()
         _, single_frame_predictions, _ = model.predict_video(video_path)
@@ -39,8 +39,14 @@ def main(args):
         prediction_time = end_time - start_time
         print(f"Prediction time: {prediction_time} seconds")
 
-        with open("video_list.txt", "a") as f:
-            f.write(f"file '{video_filename}' to video_{scene_id}.mp4 with time {prediction_time}\n")
+        current_time = time.strftime("%d%m%H:%M:%S")
+        video_info_output_path = os.path.join(
+            args.output, f"video_info_{current_time}.txt"
+        )
+        with open(video_info_output_path, "a") as f:
+            f.write(
+                f"file '{video_filename}' to video_{scene_id}.mp4 with time {prediction_time}\n"
+            )
 
         if single_frame_predictions is None:
             print(f"Failed to process video {video_path}")
@@ -50,39 +56,14 @@ def main(args):
         scenes = model.predictions_to_scenes(single_frame_predictions)
         scenes = scenes.tolist()
 
+        video_filename = video_filename.split(".")[0] + ".txt"
+        output_path = os.path.join(args.output, video_filename)
         os.makedirs(args.output, exist_ok=True)
-        split_id = 0
-        for idx, (start_frame, end_frame) in enumerate(scenes):
-            # video_name = video_filename.split(".")[0]
-            # video_name = f"{video_name}_{idx}"
-            video_name = f"video_{scene_id}_{split_id}"
-            output_video_folder = os.path.join(args.output, video_name)
-            split_id += 1
-            os.makedirs(output_video_folder, exist_ok=True)
 
-            # Clear files in video_output_folder
-            clear_dir_contents(output_video_folder)
-
-            # Save frames
-            print(f"Saving frames to {output_video_folder}...")
-            try:
-                stream = ffmpeg.input(video_path)
-                stream = ffmpeg.output(
-                    stream,
-                    f"{output_video_folder}/%06d.jpg",
-                    loglevel="quiet",
-                    start_number=0,
-                    vf=f"select='between(n,{start_frame},{end_frame})'",
-                    r=args.fps,
-                )
-                ffmpeg.run(stream, overwrite_output=True)
-            except Exception as e:
-                print(
-                    f"Failed to save frames for video {video_path} with error message: {e}"
-                )
-                failing_video.append(video_path)
-                continue
-        scene_id += 1
+        print(f"Saving scenes to {output_path}...")
+        with open(output_path, "w") as f:
+            for start_frame, end_frame in scenes:
+                f.write(f"{start_frame} {end_frame}\n")
 
     print(f"Finished")
     if len(failing_video) > 0:
