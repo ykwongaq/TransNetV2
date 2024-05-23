fc601d82238d3f7c1c6e7d3d6288929391bee685
wyk <arkwanyuk@gmail.com>
2024-05-24 03:24:00 +0800

    Roll back to the version using cv2

--- inference-pytorch/preprocessing.py
+++ inference-pytorch/preprocessing.py
@@ -4,67 +4,47 @@ import argparse
 import shutil
 
 from tqdm import tqdm
-import time
-import ffmpeg
-import subprocess
-
-from concurrent.futures import ThreadPoolExecutor
 
 
-def split_segment(input_file, start_time, segment_duration, output_file):
-    ffmpeg.input(input_file, ss=start_time, t=segment_duration).output(
-        output_file
-    ).global_args("-loglevel", "error").run(overwrite_output=True)
+def preprocess_video(video_path, output_folder, max_frames=3000):
 
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-def preprocess_video(input_file, output_folder, frames_per_segment=3000):
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    # Get video info
-    probe = ffmpeg.probe(input_file)
-    video_stream = next(
-        (stream for stream in probe["streams"] if stream["codec_type"] == "video"), None
+    num_segments = total_frames // max_frames + (
+        1 if total_frames % max_frames != 0 else 0
     )
 
-    if video_stream is None:
-        raise Exception("No video stream found")
-
-    frame_rate = eval(
-        video_stream["r_frame_rate"]
-    )  # Convert frame rate string to float
-    duration = float(video_stream["duration"])  # Total duration of the video in seconds
-    total_frames = int(frame_rate * duration)  # Calculate total frames
-
-    segment_duration = (
-        frames_per_segment / frame_rate
-    )  # Duration of each segment in seconds
-
-    segment_count = total_frames // frames_per_segment + (
-        1 if total_frames % frames_per_segment else 0
-    )
-
-    if segment_count == 1:
-        output_file = os.path.join(output_folder, f"{os.path.basename(input_file)}")
-        shutil.copy(input_file, output_file)
+    if num_segments == 1:
+        shutil.copy(video_path, output_folder)
         return
 
-    print(f"Splitting video into {segment_count} segments")
-    num_workers = args.num_workers
-    with ThreadPoolExecutor(max_workers=num_workers) as executor:
-        futures = []
-        for i in range(segment_count):
-            start_time = i * segment_duration
-            output_file = os.path.join(
-                output_folder,
-                f"{os.path.splitext(os.path.basename(input_file))[0]}_{i:03d}.mp4",
-            )
-            futures.append(
-                executor.submit(
-                    split_segment, input_file, start_time, segment_duration, output_file
-                )
-            )
+    print(f"Splitting video into {num_segments} segments")
+    for segment in range(num_segments):
+        output_file = os.path.join(
+            output_folder,
+            f"{os.path.splitext(os.path.basename(video_path))[0]}_{segment}.mp4",
+        )
+        out = cv2.VideoWriter(
+            output_file,
+            cv2.VideoWriter_fourcc(*"mp4v"),
+            fps,
+            (frame_width, frame_height),
+        )
 
-        for future in futures:
-            future.result()  # Wait for all threads to complete
+        for frame_num in range(max_frames):
+            ret, frame = cap.read()
+            if not ret:
+                break
+            out.write(frame)
+
+        out.release()
+
+    cap.release()
 
 
 def main(args):
@@ -83,7 +63,6 @@ def main(args):
     os.makedirs(output_folder)
 
     for video_filename in os.listdir(input_folder):
-
         print(f"Preprocessing video: {video_filename}")
         video_path = os.path.join(input_folder, video_filename)
         preprocess_video(video_path, output_folder, max_frames)
@@ -103,9 +82,6 @@ if __name__ == "__main__":
         type=int,
         default=3000,
         help="Maximum number of frames to be extracted. Default 3000",
-    )
-    parser.add_argument(
-        "--num_workers", type=int, default=4, help="Number of workers. Default 4"
     )
     args = parser.parse_args()
     main(args)
